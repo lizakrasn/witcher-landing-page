a39c55a89d4c2f866ff9198c0f162990a1b46c2e
Liza Krasnoshchok <nlight1122@gmail.com>
2020-11-23 01:28:44 +0200

    add Parallax effect

--- src/main.js
+++ src/main.js
@@ -1,11 +1,29 @@
-const { getElementById } = require("domutils");
-
 function copyCode () {
   const copiedCode = document.getElementById("code");
+
   copiedCode.select();
   document.execCommand("copy");
 
-  document.getElementById("copyCode").innerHTML = "COPIED";
+  document.getElementById("copyCode").innerText = "COPIED";
 }
 
 document.getElementById("copyCode").addEventListener('click', copyCode);
+
+function parallax () {
+  const parallaxBox = document.querySelector ('.page');
+  const layers = document.querySelectorAll('.background-image');
+
+  parallaxBox.addEventListener('mousemove', (event) => {
+    const x = event.clientX - parallaxBox.offsetLeft;
+
+    for (let i = 0; i < layers.length; i++) {
+      const speed = (i + 1) * 9;
+      mouseParallax(layers[i], x, speed)
+    }
+  })
+}
+function mouseParallax (layer, mouseX, speed) {
+  layer.style.transform = 'translateX(' + mouseX / speed + 'px)';
+}
+
+parallax();
