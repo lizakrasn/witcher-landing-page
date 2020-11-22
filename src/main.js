103dc741edcf649147d93973ba790b4698cce152
Liza Krasnoshchok <nlight1122@gmail.com>
2020-11-22 15:45:52 +0200

    add function copyCode

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log('Hello parcel');
+const { getElementById } = require("domutils");
+
+function copyCode () {
+  const copiedCode = document.getElementById("code");
+  copiedCode.select();
+  document.execCommand("copy");
+
+  document.getElementById("copyCode").innerHTML = "COPIED";
+}
+
+document.getElementById("copyCode").addEventListener('click', copyCode);
